6f13ec0039472d455308083a93a1a9531fbef13e
Jack Wright <28375223+jack3898@users.noreply.github.com>
2023-04-15 18:20:06 +0100

    feat(#26): added /volume command

--- apps/bot/src/commands/index.ts
+++ apps/bot/src/commands/index.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 export { About } from './About';
 export { Enqueue } from './Enqueue';
 export { Myqueue } from './Myqueue';
@@ -6,4 +5,4 @@ export { Play } from './Play';
 export { Queue } from './Queue';
 export { Quickplay } from './Quickplay';
 export { Stop } from './Stop';
-
+export { Volume } from './Volume';
