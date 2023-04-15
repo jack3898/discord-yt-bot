b46355aee03e6e185c9e41c7baa9f79c45a7f1f2
Jack Wright <28375223+jack3898@users.noreply.github.com>
2023-04-15 23:44:03 +0100

    fix(#10): fixed seeder not working

--- apps/seeder/src/index.ts
+++ apps/seeder/src/index.ts
@@ -1,14 +1,12 @@
-import { PrismaClient } from '@yt-bot/database';
 import { RESOURCE_TYPES } from '@yt-bot/constants';
-
-const seederClient = new PrismaClient();
+import { prismaClient } from '@yt-bot/database';
 
 // will be expanded in the future
 async function main() {
-	await seederClient.resourceType.deleteMany();
+	await prismaClient.resourceType.deleteMany();
 
-	await seederClient.$transaction([
-		seederClient.resourceType.create({
+	await prismaClient.$transaction([
+		prismaClient.resourceType.create({
 			data: { name: RESOURCE_TYPES.YOUTUBE_VIDEO }
 		})
 	]);
